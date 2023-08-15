497aa165f2093881ed9843fc0818cd66140ac9b5
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-08-15 14:15:17 +0900

    2.3

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,16 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+
+const superheroes = require('superheroes');
+var superHero = superheroes.random();
+
+console.log(`I am ${superHero}!`);
+
+
+
+
