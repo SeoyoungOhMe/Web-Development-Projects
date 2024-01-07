b48f1c0b034f234385ad46429ebb4882f56aedc2
Seoyoung Oh <ohseoyoung5@gmail.com>
2024-01-07 19:57:29 +0900

    Add files via upload

---
+++ 36.11 React Components/src/components/App.jsx
@@ -0,0 +1,13 @@
+import Heading from "./Heading";
+import List from "./List";
+import React from "react";
+
+function App() {
+    return <div>
+        <Heading />
+        <List />
+        <List />
+    </div>
+}
+
+export default App;
