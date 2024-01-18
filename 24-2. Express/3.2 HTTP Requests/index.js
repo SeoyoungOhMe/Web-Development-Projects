c90e3b391e93390bc749f1152bf0671c2b40d274
Seoyoung Oh <ohseoyoung5@gmail.com>
2024-01-19 01:07:28 +0900

    202

--- 24-2. Express/3.2 HTTP Requests/index.js
+++ 24-2. Express/3.2 HTTP Requests/index.js
@@ -3,17 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
-});
+    res.send("<h1>Hello</h1>");
+})
 
 app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
-});
+    res.send("<h1>010-1234-5678</h1>");
+})
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.get("/about", (req, res) => {
+    res.send("<h1>Hi I'm OSY!</h1>");
+})
+
+app.listen(3000, () => {
+    console.log(`Server running on port ${port}.`);
+})
